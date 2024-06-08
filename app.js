444aa80e238431ae68cd4f48a5a6ecf03100daf5
Raylan Cordeiro de Souza <raylan.cordeiro@DIGITALCOLLEGE.LOCAL>
2024-06-07 21:40:30 -0300

    Implementa rota conta

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import express from "express"
 const app = express()
 const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/conta/:id', (req, res) => {
+    res.json({
+        conta_id: 1234,
+        saldo: 18970
+    })
 })
 
+app.post('/conta', (req, res) => {
+
+    const { conta_id, saldo } = req.body
+    if (!conta_id || !saldo) {
+        res.status(400).json({message: "Os campos conta e saldo obrigatóriods"})
+    }
+
+    res.status(201).json({
+        conta_id: 1234,
+        saldo: 18970
+    })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
