338dae380b4ab31bda705dd5be3c0a58a761e83b
Raylan Cordeiro de Souza <raylan.cordeiro@DIGITALCOLLEGE.LOCAL>
2024-06-14 21:19:41 -0300

    Implementa banco de dados

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+import "dotenv/config"
 import express from "express"
+import mysql from 'mysql'
 const app = express()
 const port = 3000
 app.use(express.json())
 
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD
+});
+
 app.get('/conta/:id', (req, res) => {
-    res.json({
-        conta_id: 1234,
-        saldo: 18970
-    })
+
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+        if (err) throw err;
+        res.json({
+            conta_id: 1234,
+            saldo: 18970
+        })
+      });
+    
 })
 
 app.post('/conta', (req, res) => {
