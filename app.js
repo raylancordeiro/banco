d31d85ae4ee50842507fc4aba8333ec45029e943
Raylan Cordeiro <raylancordeiro@hotmail.com>
2024-06-21 21:20:23 -0300

    Prepare to deploy

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var connection = mysql.createConnection({
   host     : process.env.DB_HOST,
   database : process.env.DB_NAME,
   user     : process.env.DB_USER,
-  password : process.env.DB_PASSWORD
+  password : process.env.DB_PASSWORD,
+  port     : process.env.DB_PORT
 });
 
 app.get('/conta/:id', (req, res) => {
